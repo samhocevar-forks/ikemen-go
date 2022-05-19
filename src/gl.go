316525e3d426fc9304cf65a3dbab7b664fd4425c
Sam Hocevar <sam@hocevar.net>
2022-05-19 10:25:47 +0200

    Track texture memory allocations and deallocations

--- src/gl.go
+++ src/gl.go
@@ -8,26 +8,31 @@ import (
 
 type Texture struct {
 	handle gl.Texture
+	size float64
 }
 
 // Generate a new texture name
 func newTexture() (t *Texture) {
-	t = &Texture{gl.CreateTexture()}
+	t = &Texture{handle: gl.CreateTexture()}
+	UpdateStat(StatTextureCount, 1)
 	runtime.SetFinalizer(t, (*Texture).finalizer)
 	return
 }
 
 // Bind a texture and upload texel data to it
 func (t *Texture) SetData(width, height, depth int32, filter bool, data []byte) {
-	var interp int = gl.NEAREST
+	interp := int(gl.NEAREST)
 	if filter {
 		interp = gl.LINEAR
 	}
 
-	var format gl.Enum = gl.LUMINANCE
+	size := float64(width * height) / (1024 * 1024)
+	format := gl.Enum(gl.LUMINANCE)
 	if depth == 24 {
+		size *= 4
 		format = gl.RGB
 	} else if depth == 32 {
+		size *= 4
 		format = gl.RGBA
 	}
 
@@ -39,9 +44,15 @@ func (t *Texture) SetData(width, height, depth int32, filter bool, data []byte) 
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, interp)
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+
+	// Update texture stats
+	UpdateStat(StatTextureMemory, size - t.size)
+	t.size = size
 }
 
 func (t *Texture) finalizer() {
+	UpdateStat(StatTextureCount, -1)
+	UpdateStat(StatTextureMemory, -t.size)
 	sys.mainThreadTask <- func() {
 		gl.DeleteTexture(t.handle)
 	}
